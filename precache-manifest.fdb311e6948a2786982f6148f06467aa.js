c4632ad6549fd41a6237a597749f9572f534d318
aleksey34 <33332731+aleksey34@users.noreply.github.com>
2020-03-01 23:39:31 +0300

    First Deploy to github

---
+++ precache-manifest.fdb311e6948a2786982f6148f06467aa.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "faf96e178321f5042456cfd0d6e27bf0",
+    "url": "/index.html"
+  },
+  {
+    "revision": "99a8485efed468234f71",
+    "url": "/static/css/main.e512bb4d.chunk.css"
+  },
+  {
+    "revision": "9eee67cf782a38d35aeb",
+    "url": "/static/js/2.d6a534e5.chunk.js"
+  },
+  {
+    "revision": "99a8485efed468234f71",
+    "url": "/static/js/main.80735a1c.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
